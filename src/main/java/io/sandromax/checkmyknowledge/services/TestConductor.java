c423ee9cf4299ae25113d30908cac6a4786307af
sandromax <sandrondk@gmail.com>
2019-03-04 18:24:15 +0200

    create TestConductor class for providing process of testing

---
+++ src/main/java/io/sandromax/checkmyknowledge/services/TestConductor.java
@@ -0,0 +1,39 @@
+package io.sandromax.checkmyknowledge.services;
+
+import io.sandromax.checkmyknowledge.domain.Issue;
+import io.sandromax.checkmyknowledge.domain.Result;
+import io.sandromax.checkmyknowledge.storage.IssueBase;
+import io.sandromax.checkmyknowledge.storage.TestResults;
+import io.sandromax.checkmyknowledge.services.test_loaders.impl.SimpleTestLoader;
+
+import java.io.IOException;
+
+public class TestConductor {
+    private IssueBase issueBase;
+    private TestResults testResults;
+
+    public void init(String path) {
+        try {
+            SimpleTestLoader loader = new SimpleTestLoader();
+            issueBase = new IssueBase(loader.load(path));
+
+            testResults = new TestResults();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public Issue getNewIssue() {
+        return issueBase.getRandomIssue();
+    }
+
+    public void saveIssueDone(Issue issue, String answer) {
+        testResults.addResult(new Result(issue, answer));
+    }
+
+    public void endTest() {
+        
+    }
+}
