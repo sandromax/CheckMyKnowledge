c1443d0b0e3377d81bc3c5fe1e00d5ccfa372924
sandromax <sandrondk@gmail.com>
2019-03-20 13:41:38 +0200

    Change IssueBase class with non repeat features. Delete class TestResult

--- src/main/java/io/sandromax/checkmyknowledge/storage/IssueBase.java
+++ src/main/java/io/sandromax/checkmyknowledge/storage/IssueBase.java
@@ -1,13 +1,13 @@
 package io.sandromax.checkmyknowledge.storage;
 
 import io.sandromax.checkmyknowledge.domain.Issue;
+import io.sandromax.checkmyknowledge.exceptions.NoNewIssuesInBase;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Random;
+import java.util.*;
 
 public class IssueBase {
-    private HashMap<String, Issue> issueBase;
+    private HashMap<String, Issue> issueBase = new HashMap<>();
+    private List<Issue> pastIssues = new ArrayList<>();
 
     public IssueBase(HashSet<Issue> issues) {
         for(Issue issue : issues) {
@@ -17,11 +17,22 @@ public class IssueBase {
         }
     }
 
-    public Issue getRandomIssue() {
-        Random generator = new Random();
-        Object[] values = issueBase.values().toArray();
-        Object randomValue = values[generator.nextInt(values.length)];
+    public Issue getRandomIssue() throws NoNewIssuesInBase{
+        if(issueBase.size() == pastIssues.size()) {
+            throw new NoNewIssuesInBase("issue base is empty");
+        }
 
-        return (Issue)randomValue;
+        Random generator = new Random();
+        Object[] keys = issueBase.keySet().toArray();
+        Object randomKey = keys[generator.nextInt(keys.length)];
+
+        while(pastIssues.contains(issueBase.get(randomKey))) {
+            randomKey = keys[generator.nextInt(keys.length)];
+        }
+
+        Issue result = issueBase.get(randomKey);
+        pastIssues.add(result);
+
+        return result;
     }
 }
